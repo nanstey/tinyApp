29314e1d37d43c729970bc1230a6f9cdb955418e
Noel Anstey <anstey.ng@gmail.com>
2017-03-15 18:24:34 +0000

    init commit, forgot to start repo earlier ...

---
+++ express_server.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+const randomstring = require("randomstring");
+
+var PORT = process.env.PORT || 8080; // default port 8080
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls/:id", (req, res) => {
+  console.log(req.body);
+  let shortURL = req.params.id;
+  let longURL = req.body['longURL'];
+  urlDatabase[shortURL] = longURL;
+  res.redirect('/urls/')
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  let shortURL = generateRandomString();
+  let longURL = req.body['longURL'];
+  urlDatabase[shortURL] = longURL;
+  res.redirect('/urls/' + shortURL);
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  let shortURL = req.params.id;
+  delete urlDatabase[shortURL];
+  res.redirect('/urls/');
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  let longURL = urlDatabase[shortURL];
+
+  res.redirect(longURL);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+function generateRandomString() {
+  return randomstring.generate(6);
+}
+
